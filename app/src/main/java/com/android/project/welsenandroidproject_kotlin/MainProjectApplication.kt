f589dabfeafb619a09f4da210024e7660364aef8
TTXD19 <welsen9595@gmail.com>
2021-03-26 12:07:56 +0800

    feat(develop): Add MainProjectApplication.kt for dagger-hilt

---
+++ app/src/main/java/com/android/project/welsenandroidproject_kotlin/MainProjectApplication.kt
@@ -0,0 +1,8 @@
+package com.android.project.welsenandroidproject_kotlin
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class MainProjectApplication: Application() {
+}
