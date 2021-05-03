2b648ebc95427c6d8a612d0726f64129706a12f0
TTXD19 <welsen9595@gmail.com>
2021-05-03 13:30:52 +0800

    feature(CleanCodeArchi/spaceX_api_connect): Build base MainActivity.kt

--- app/src/main/java/com/android/project/welsenandroidproject_kotlin/MainActivity.kt
+++ app/src/main/java/com/android/project/welsenandroidproject_kotlin/MainActivity.kt
@@ -1,20 +1,30 @@
 package com.android.project.welsenandroidproject_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import dagger.hilt.EntryPoint
+import com.android.project.welsenandroidproject_kotlin.databinding.ActivityMainBinding
+import com.android.project.welsenandroidproject_kotlin.ui.base.BaseActivity
+import com.android.project.welsenandroidproject_kotlin.ui.base.BaseViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity<ActivityMainBinding>() {
 
     private val viewModel: MainActivityViewModel by viewModels()
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+    override fun initViewBinding(): ActivityMainBinding {
+        return ActivityMainBinding.inflate(layoutInflater)
+    }
 
+    override fun getLayoutId(): Int = R.layout.activity_main
+
+    override fun getViewModel(): BaseViewModel? = viewModel
+
+    override fun initParam(data: Bundle) {
+
+    }
+
+    override fun initView(savedInstanceState: Bundle?) {
         viewModel.getData()
     }
 }
--- app/src/main/java/com/android/project/welsenandroidproject_kotlin/ui/base/BaseActivity.kt
+++ app/src/main/java/com/android/project/welsenandroidproject_kotlin/ui/base/BaseActivity.kt
@@ -1,40 +1,52 @@
 package com.android.project.welsenandroidproject_kotlin.ui.base
 
+import android.database.ContentObserver
 import android.os.Bundle
 import android.os.PersistableBundle
+import android.widget.Toast
+import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentTransaction
 import androidx.viewbinding.ViewBinding
+import io.reactivex.rxjava3.disposables.CompositeDisposable
 
 abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
-
+    protected lateinit var params: Bundle
+    protected val compositeDisposable: CompositeDisposable = CompositeDisposable()
+    private var toast: Toast? = null
+    private var screenshotObserver: ContentObserver? = null
     protected var binding: VB? = null
 
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(getLayoutId())
+        params = intent.extras ?: Bundle.EMPTY
+        initParam(params)
         binding = initViewBinding().apply {
             setContentView(root)
         }
         initView(savedInstanceState)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
     }
 
     override fun onDestroy() {
+        compositeDisposable.dispose()
+        if (screenshotObserver != null) {
+            contentResolver.unregisterContentObserver(screenshotObserver!!)
+        }
+        binding = null
         super.onDestroy()
     }
-
-    protected abstract fun initParam(data: Bundle)
-
-    protected abstract fun getViewModel(): BaseViewModel
-
-    protected abstract fun initViewBinding(): VB
-
-    protected abstract fun initView(savedInstanceState: Bundle?)
 
     protected fun addFragment(
         containerId: Int,
         fragment: Fragment,
-        tag: String? = fragment::class.java.name
+        tag: String? = fragment::class.java.name,
     ): FragmentTransaction {
         return supportFragmentManager.beginTransaction()
             .addFragment(containerId, fragment, tag)
@@ -43,10 +55,29 @@ abstract class BaseActivity<VB: ViewBinding>: AppCompatActivity() {
     protected fun FragmentTransaction.addFragment(
         containerId: Int,
         fragment: Fragment,
-        tag: String? = fragment::class.java.name
+        tag: String? = fragment::class.java.name,
     ): FragmentTransaction {
         return this.apply {
             this.add(containerId, fragment, tag)
         }
     }
+
+    protected fun FragmentTransaction.safeCommit() {
+        if (supportFragmentManager.isStateSaved) {
+            this.commitAllowingStateLoss()
+        } else {
+            this.commit()
+        }
+    }
+
+    protected abstract fun initViewBinding(): VB
+
+    @LayoutRes
+    protected abstract fun getLayoutId(): Int
+
+    protected abstract fun getViewModel(): BaseViewModel?
+
+    protected abstract fun initParam(data: Bundle)
+
+    protected abstract fun initView(savedInstanceState: Bundle?)
 }
